5dd2edfaf95909359a142b977d3cdd57638910d3
LAntonioRibeiroD <toni101ribeiro@gmail.com>
2023-11-25 21:41:46 +0200

    laying the ground work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LandingSectionComponent } from './components/landing-section/landing-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LandingSectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
